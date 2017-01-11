1ad1bd10c3729e25447033493380c44598e87acf
Roman Tiskiy <roman@pacto.io>
2017-01-11 17:06:35 +0200

    Server

--- main.js
+++ main.js
@@ -1,37 +1,9 @@
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-
-// listener #1
-var listner1 = function listner1() {
-   console.log('listner1 executed.');
+function printHello(){
+   console.log( "Hello, World!");
 }
 
-// listener #2
-var listner2 = function listner2() {
-  console.log('listner2 executed.');
-}
+// Now call above function after 2 seconds
+var t = setTimeout(printHello, 2000);
 
-// Bind the connection event with the listner1 function
-eventEmitter.addListener('connection', listner1);
-
-// Bind the connection event with the listner2 function
-eventEmitter.on('connection', listner2);
-
-var eventListeners = require('events').EventEmitter.listenerCount
-   (eventEmitter,'connection');
-console.log(eventListeners + " Listner(s) listening to connection event");
-
-// Fire the connection event 
-eventEmitter.emit('connection');
-
-// Remove the binding of listner1 function
-eventEmitter.removeListener('connection', listner1);
-console.log("Listner1 will not listen now.");
-
-// Fire the connection event 
-eventEmitter.emit('connection');
-
-eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
-console.log(eventListeners + " Listner(s) listening to connection event");
-
-console.log("Program Ended.");
+// Now clear the timer
+clearTimeout(t);

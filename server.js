261786f15ff668c35d49a033a4f4496be614f67d
Aaron E <aaron@pacto.io>
2017-01-13 09:01:47 +0000

    Fixed upload issues

--- server.js
+++ server.js
@@ -3,39 +3,36 @@ var app = express();
 var fs = require("fs");
 
 var bodyParser = require('body-parser');
-var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' })
-
-var app = express()
+var multer = require('multer');
+var upload = multer({ dest: '/tmp/'});
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(multer({ dest: '/tmp/'}));
-//app.use(multer({ dest: 'uploads/'}));
 
 app.get('/index.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
-})
+});
 
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
-  // req.file is the `avatar` file 
-  // req.body will hold the text fields, if there were any 
-})
- 
-app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
-  // req.files is array of `photos` files 
-  // req.body will contain the text fields, if there were any 
-})
- 
-var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
-app.post('/cool-profile', cpUpload, function (req, res, next) {
-  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files 
-  // 
-  // e.g. 
-  //  req.files['avatar'][0] -> File 
-  //  req.files['gallery'] -> Array 
-  // 
-  // req.body will contain the text fields, if there were any 
+app.post('/file_upload', upload.single('file'), function (req, res) {
+   console.log(req.file.name);
+   console.log(req.file.path);
+   console.log(req.file.type);
+   var file = __dirname + "/" + req.file.name;
+   
+   fs.readFile( req.file.path, function (err, data) {
+      fs.writeFile(file, data, function (err) {
+         if( err ){
+            console.log( err );
+            }else{
+               response = {
+                  message:'File uploaded successfully',
+                  filename:req.file.name
+               };
+            }
+         console.log( response );
+         res.end( JSON.stringify( response ) );
+      });
+   });
 })
 
 var server = app.listen(8081, function () {
